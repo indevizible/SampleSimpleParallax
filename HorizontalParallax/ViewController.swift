f28258a1510523c9bd40e2827c610db77cb85cb9
Nattawut Singhchai <jadedragon17650@gmail.com>
2016-01-29 09:49:08 +0700

    Initial Commit

---
+++ HorizontalParallax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorizontalParallax
+//
+//  Created by Nattawut Singhchai on 1/29/16.
+//  Copyright © 2016 Nattawut Singhchai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
