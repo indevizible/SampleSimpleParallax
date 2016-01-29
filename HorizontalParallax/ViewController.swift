b6c004dd8b55d57c5f8322232cf3363867149218
Nattawut Singhchai <jadedragon17650@gmail.com>
2016-01-29 11:11:40 +0700

    sample simple parallax

--- HorizontalParallax/ViewController.swift
+++ HorizontalParallax/ViewController.swift
@@ -8,7 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Cell: UICollectionViewCell {
+    
+    private let parallaxRatio:CGFloat = -1.2
+    private let parallaxRatioL2: CGFloat = -5.0
+    
+    @IBOutlet var horizontalConstraints: [NSLayoutConstraint]!
+    
+    @IBOutlet var horizontalConstraintsL2: [NSLayoutConstraint]!
+    
+    override func drawRect(rect: CGRect) {
+        clipsToBounds = false
+        contentView.clipsToBounds = false
+        
+        var tmpView:UIView? = self
+        while let view = tmpView {
+            if view.clipsToBounds {
+                view.clipsToBounds = false
+            }
+            tmpView = view.superview
+        }
+    }
+    
+    func didScroll(collectionView: UICollectionView) {
+        
+        let offsetX = (collectionView.contentOffset.x - frame.minX)
+        
+        for horizontalConstraint in horizontalConstraints {
+            horizontalConstraint.constant = offsetX * parallaxRatio
+        }
+        
+        for horizontalConstraint in horizontalConstraintsL2 {
+            horizontalConstraint.constant = offsetX * parallaxRatioL2
+        }
+    }
+}
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +55,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCellWithReuseIdentifier("reusableIdentifier", forIndexPath: indexPath)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
 
 
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        guard let indexPaths  = collectionView?.indexPathsForVisibleItems() else{
+            return
+        }
+        for indexPath in indexPaths {
+            guard let cell = collectionView?.cellForItemAtIndexPath(indexPath) as? Cell else {
+                return
+            }
+            cell.didScroll(collectionView!)
+        }
+    }
 }
-
